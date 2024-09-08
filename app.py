e7bcd6e65fc77199faa3428e2341c3dea1d1fdb5
gnp <107291366+GouthamOfTheNP@users.noreply.github.com>
2024-09-08 17:47:36 -0500

    Updated code

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ def home():
         return "An error occurred while processing your request.", 500
 
 
-@app.route('/v1.0')
+@app.route('/v1.1')
 def changelog():
     try:
         return render_template("changelog.html")
@@ -22,22 +22,31 @@ def changelog():
         return "An error occurred while processing your request.", 500
 
 
-@app.route('/v1.0/<a>_<b>_<c>')
+@app.route('/v1.1/<a>_<b>_<c>')
 def solutions(a, b, c):
     try:
         try:
             a = float(a)
             b = float(b)
             c = float(c)
-            positive = (-b+math.sqrt((b**2)-(4*a*c)))
-            negative = (-b-math.sqrt((b**2)-(4*a*c)))
         except ValueError:
-            positive = "Not real solution"
-            negative = "Not real solution"
+            positive = "Cannot calculate due to non-integer types"
+            negative = "Cannot calculate due to non-integer types"
+        else:
+            try:
+                positive = (-b+math.sqrt((b**2)-(4*a*c)))
+                negative = (-b-math.sqrt((b**2)-(4*a*c)))
+            except ValueError:
+                positive = "Not real solution"
+                negative = "Not real solution"
         return render_template("solutions.html", entered_data=[a, b, c], positive=positive, negative=negative)
     except Exception as e:
         print(f"Error occurred: {e}")
         return "An error occurred while processing your request.", 500
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
 
 
 if __name__ == '__main__':
